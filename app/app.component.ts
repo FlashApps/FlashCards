cbb5af8a6a15716407374d8a4a5678b946457dee
Tim Musgrove <tim.musgrove91@gmail.com>
2016-08-15 15:50:33 -0600

    Adding landing page images

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
 import {HeaderComponent} from './header.component';
+import {LandingComponent} from './landing.component';
 
 @Component({
     selector: 'my-app',
-    template: '<my-header></my-header>'
-    directives: [HeaderComponent]
+    template: '<my-header></my-header><landing-page></landing-page>',
+    directives: [HeaderComponent, LandingComponent]
 })
 export class AppComponent { }
