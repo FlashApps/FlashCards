c23a6cff75c9fc739817020ce63fe3a7d4ce2a66
Bennett Joerger <bennettjoerger@gmail.com>
2016-08-18 14:05:33 -0600

    Landing Page updates

--- app/landing.component/landing.component.ts
+++ app/landing.component/landing.component.ts
@@ -9,13 +9,14 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
         <div class="row landingImage">
             <div class="col-md-4 landingText">
                 <h2>Learn Anything</h2>
-                <h3>AS NATURALLY AS LEARNING TO WALK</h3>
+                <h3>AS NATURALLY AS LEARNING TO WALK...</h3>
             </div>
             <div class="col-lg-8">
 
             <img src="https://adoptresources.files.wordpress.com/2010/09/learning-istock_000011456468.jpg" />
         </div>
         </div>
+        <br><br>
         <a [routerLink]="['Search']"> <button class="btn btn-lg btn-primary" >GET STARTED</button></a>
 
     </main>
