adac4780cbf967149e8df2e0c22fec2c99459e19
Tim Musgrove <tim.musgrove91@gmail.com>
2016-08-18 10:18:08 -0600

    Adding highlighting

--- app/flashcard.component/flashcard.component.ts
+++ app/flashcard.component/flashcard.component.ts
@@ -16,17 +16,15 @@ import {StopTimer} from '../StopTimer/StopTimer';
             <div class="row">
                 <div class="col-lg-3 col-md-3 hidden-sm hidden-xs well flashCardSidebar">
                 <ul>
-                  <li *ngFor="#card of deck.terms">{{card.term}}</li>
+                  <li class="cardDef" (click)="highlight(index)" *ngFor="#card of deck.terms; #index = index" [class.highlighted]="index == highlightedIndex">{{card.term}}</li>
                 </ul>
                 </div>
                 <div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
                 <div class="col-lg-8 col-md-8 well flashCardContainer">
-                <h3>Deck: {{deck.title}}</h3>
-                <ul>
-
-
-                </ul>
-                  <a><p>Click to Flip!</p></a>
+                    <div id="card">
+                        <figure class="front">hadfhadffsdfasfasdfasdfasdfasdfasdfasdfasdfasdf</figure>
+                        <figure class="back">gasdfasdfasdfasdgadfgafgasgadsgsdgasgasggasdgasdgasg</figure>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -46,7 +44,7 @@ import {StopTimer} from '../StopTimer/StopTimer';
                   <span class="glyphicon glyphicon-menu-left
                   " aria-hidden="true"></span> Last
                   </button>
-                  <button type="button" class="btn btn-default btn-lg">
+                  <button type="button" (click)="toggleFlip()" class="btn btn-default btn-lg">
                     <span class="glyphicon glyphicon-play" aria-hidden="true"></span> Flip
                   </button>
                   <button type="button" class="btn btn-default btn-lg">
@@ -62,6 +60,9 @@ export class FlashcardComponent implements OnInit{
   deckId: Object;
   deck: Object = {};
   params: string;
+  currentFront: string;
+  currentBack: string;
+  highlightedIndex: number = 0;
 
   constructor (params: RouteParams){
           this.deckId = params.get("id");
@@ -69,15 +70,25 @@ export class FlashcardComponent implements OnInit{
         }
 
     loadPage(){
-      console.log("hi tim");
       $('#secret').hide();
         $('#stop').show();
+      $('.cardDef').click(function() {
+            $(this).addClass("highlighted")
+        })
       var url = "http://galvanize-cors-proxy.herokuapp.com/https://api.quizlet.com/2.0/sets/" + this.deckId + "?client_id=BGDhWP7Cth&whitespace=1";
       $.get(url).done( (data) => {
           this.deck = data;
           console.log(this.deck);
       });
 
+    }
+
+    highlight(index){
+        this.highlightedIndex = index;
+    }
+
+    toggleFlip(){
+        $('#card').toggleClass("flipped");
     }
 
   ngOnInit() {
